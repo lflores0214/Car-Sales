84cb96bda8a9019868afd436972eb19eb8f73e4a
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-06 14:21:04 -0700

    set up an empty reducer and initial state

---
+++ src/reducers/reducer.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+export const reducer = (state, action) => {
+    switch (action.type){
+
+    }
+}
+
+export const initialState = {
+    addtionalPrice: 0,
+    car: {
+        price: 26395,
+        name: '2019 Ford Mustang',
+        image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+        features: []
+    },
+    addionalFeatures: [
+        { id:1, name: 'V-6 engine', price: 1500 },
+        { id:2, name: 'Racing deatail package', price: 1500 },
+        { id:3, name: 'Premium sound system', price: 500 },
+        { id:4, name: 'Rear spoiler', price: 250 }
+    ]
+}
